3c4c40c0bf7420a95dbd4427b4b92ab086210890
Wiebe Rinsma <wiebe.rinsma@han.nl>
2024-09-11 11:46:03 +0200

    Working jwt spring boot 3, spring security 6

--- src/main/java/nl/han/oose/project/sb3/entities/User.java
+++ src/main/java/nl/han/oose/project/sb3/entities/User.java
@@ -1,29 +1,28 @@
 package nl.han.oose.project.sb3.entities;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "_user")
+@Table(name = "user")
 public class User implements UserDetails
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
     private String firstName;
 
